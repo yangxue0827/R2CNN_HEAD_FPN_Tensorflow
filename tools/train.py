472390e96daa8acc90f0351c43cffbd3a0ae5f20
yangxue0827 <yangxue0827@126.com>
2018-03-10 16:33:38 +0800

    update

--- tools/train.py
+++ tools/train.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 import sys
-sys.path.append('../')
+sys.path.append('../') 
 
 import tensorflow.contrib.slim as slim
 import os
